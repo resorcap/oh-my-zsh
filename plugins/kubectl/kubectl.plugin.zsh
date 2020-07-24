82ba4b549eb9b658ddbbcc1d7a90ac92f866244c
cyan.guo <cyan.guo@nio.com>
2020-07-24 11:13:14 +0800

    ksync

--- plugins/kubectl/kubectl.plugin.zsh
+++ plugins/kubectl/kubectl.plugin.zsh
@@ -13,7 +13,11 @@ alias k=kubectl
 alias kaf='k apply -f'
 
 # Drop into an interactive terminal on a container
-alias keti='k exec -ti'
+function keti() {
+  pod=$1
+  command=$2
+  kubectl exec -ti $pod -- $command
+}
 
 # Manage configuration quickly to switch contexts between local, dev ad staging.
 alias kcuc='k config use-context'
@@ -21,11 +25,24 @@ alias kcsc='k config set-context'
 alias kcdc='k config delete-context'
 alias kccc='k config current-context'
 
+# Trainingjob man.
+alias kgt='k get trainingjob'
+alias kdelt='k delete trainingjob'
+alias kdelta="kgt | grep -v NAME | awk '{print \$1}' | xargs kubectl delete trainingjob"
+
 # Pod management.
 alias kgp='k get pods'
 alias kep='k edit pods'
 alias kdp='k describe pods'
 alias kdelp='k delete pods'
+alias kdelpa="kgp | grep -v NAME | awk '{print \$1}' | xargs kubectl delete pods"
+
+# Job management.
+alias kgj='k get job'
+alias kdj='k describe job'
+alias kdelj='k delete job'
+alias kdelja="kgj | grep -v NAME | awk '{print \$1}' | xargs kubectl delete job"
+
 
 # Service management.
 alias kgs='k get svc'
@@ -54,3 +71,11 @@ alias kru='k rollout undo'
 # Logs
 alias kl='k logs'
 alias klf='k logs -f'
+
+zsh_kubectl_plugin_path="$(dirname "$0")"
+function ksync() {
+  src=$1
+  dest=$2
+  rsync -ravhPC -e "${zsh_kubectl_plugin_path}/rsync-helper.sh" ${src} ${dest}
+}
+
